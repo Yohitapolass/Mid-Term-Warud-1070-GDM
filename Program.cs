823f2c8eae0280d32e3083f45bcca6c192235fc3
Yohitapolass <warud.seso@mail.kmutt.ac.th>
2023-03-13 15:32:18 +0700

    Edit Code

--- Program.cs
+++ Program.cs
@@ -10,41 +10,24 @@ namespace Delivery{
             Console.Write("Please Enter your current time : ");
             int time = int.Parse(Console.ReadLine());
 
-            Console.Write("Please Enter your order : ");
-            string order = (Console.ReadLine());
-
             int BF = 5;
             int WS = 2;
             int CF = 3;
 
-            switch(order) {
-            case "Breakfast Set":
+            while(order == "Breakfast Set"){
 
-                while(BF > 0){
                    Console.Write("Your Order is Correct!");
+                   Console.Write("Please Enter your order : ");
+                string order = (Console.ReadLine());
                    BF -= 1;
-                   return Console.Write("Please Enter your order : ");
-            string order = (Console.ReadLine());
+                   
                    
                 }
-                
-                break;
-            case "Weekend Set":
-                
 
-                break;
-            case "Coffee":
-                
-
-                break;    
-            default:
-                Console.WriteLine("incorrect");
-                break;
-
+            
 
             
 
             }
         }
     }
-}
